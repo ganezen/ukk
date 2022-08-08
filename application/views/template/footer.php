a7b7072bcf8fa28df76e7d975bb5528f0eb23385
ganezen <ganezmuzaffar@gmail.com>
2022-08-09 03:43:39 +0700

    ukk

---
+++ application/views/template/footer.php
@@ -0,0 +1,23 @@
+     <!-- AWALAN FOOTER-->
+     <aside class="aside-menu">
+    </div>
+    </aside>
+    <footer class="app-footer">
+      <div>
+        <a href="#">LSP</a>
+        <span>&copy; 2022 .</span>
+      </div>
+      <div class="ml-auto">
+        <span>DI</span>
+        <a href="#">Universitas Gunadarma</a>
+      </div>
+    </footer>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/pace-progress/pace.min.js"></script>
+    <script src="node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+    <script src="node_modules/@coreui/coreui/dist/js/coreui.min.js"></script>
+  </body>
+</html>
+<!-- AKHIR FOOTER-->
